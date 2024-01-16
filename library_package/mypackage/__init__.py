b97ca1b1a841dcea873a8948cbe78fc3113e54df
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2024-01-16 17:20:24 +0530

    Add files via upload

---
+++ library_package/mypackage/__init__.py
@@ -0,0 +1,8 @@
+# My package is here
+
+class mypack:
+    def __init__(self):
+        print("This is my constructor")
+    def func(num):
+        print("This is a function")
+        return num
---
+++ library_package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(name="mypackage",
+      version="0.2",
+      description="This is my package",
+      long_description="This is only my package",
+      author="tarun",
+      packages=['mypackage'],
+      install_requires=[])
